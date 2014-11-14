26193a43b2dc6ab5b214a71d59d24bd37ced5f0e
rafanime <rafael.lourenco.nunes@gmail.com>
2014-11-14 18:32:34 +0000

    teste

--- Lost/src/find/service/gcm/DemoActivity.java
+++ Lost/src/find/service/gcm/DemoActivity.java
@@ -165,6 +165,8 @@ public class DemoActivity extends Activity {
 							android.content.Context.MODE_PRIVATE);
 			int idRadioButton = preferences.getInt("associationState", 2);
 			ui = new Handler();
+			
+			
 			WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 			WifiInfo info = manager.getConnectionInfo();
 			
@@ -196,36 +198,18 @@ public class DemoActivity extends Activity {
 						}
 					});
 
-			// Check device for Play Services APK. If check succeeds, proceed
-			// with
-			// GCM registration and get active simulations. 
-			if (checkPlayServices()) {
-				gcm = GoogleCloudMessaging.getInstance(this);
-				regid = getRegistrationId(context);
-
-				if (regid.isEmpty()) {
-					registerInBackground();
-				} else {
-					RequestServer.register(address, regid);
-
-				}
-				
-				//registers user for simulation if intent equals "registerParticipant"
-				Intent intent = getIntent();
-				String action = intent.getAction();
-				if (action != null && action.equals("registerParticipant")) {
-					RequestServer.registerForSimulation(intent.getStringExtra("name"),  regid, address);
-					Log.d("debugg", "Register for simulation 0");
-				}
-				
-				//populates the active simulations window
-				getActiveSimulations();
-
-				Log.d(TAG, regid);
-
-			} else {
-				Log.i(TAG, "No valid Google Play Services APK found.");
+			//registers user for simulation if intent equals "registerParticipant"
+			Intent intent = getIntent();
+			String action = intent.getAction();
+			if (action != null && action.equals("registerParticipant")) {
+				RequestServer.registerForSimulation(intent.getStringExtra("name"),  regid, address);
+				Log.d("debugg", "Register for simulation 0");
 			}
+			
+			//populates the active simulations window
+			getActiveSimulations();
+			
+	
 		}
 	}
 	
@@ -384,47 +368,6 @@ public class DemoActivity extends Activity {
 
 	}
 
-	/**
-	 * Registers the application with GCM servers asynchronously.
-	 * <p>
-	 * Stores the registration ID and the app versionCode in the application's
-	 * shared preferences.
-	 */
-	private void registerInBackground() {
-		new AsyncTask<Void, Void, String>() {
-			@Override
-			protected String doInBackground(Void... params) {
-				String msg = "";
-				try {
-					if (gcm == null) {
-						gcm = GoogleCloudMessaging.getInstance(context);
-					}
-					if (regid.isEmpty())
-						regid = gcm.register(SENDER_ID);
-
-					msg = "Device registered, registration ID=" + regid;
-
-					RequestServer.register(address, regid);
-
-					// For this demo: we don't need to send it because the
-					// device will send
-					// upstream messages to a server that echo back the message
-					// using the
-					// 'from' address in the message.
-
-					// Persist the regID - no need to register again.
-					storeRegistrationId(context, regid);
-				} catch (IOException ex) {
-					msg = "Error :" + ex.getMessage();
-					// If there is an error, don't just keep trying to register.
-					// Require the user to click a button again, or perform
-					// exponential back-off.
-				}
-				return msg;
-			}
-
-		}.execute(null, null, null);
-	}
 	
 	/**
 	 * Starts/Stops the service 
@@ -597,81 +540,12 @@ public class DemoActivity extends Activity {
 		return true;
 	}
 
-	/**
-	 * Stores the registration ID and the app versionCode in the application's
-	 * {@code SharedPreferences}.
-	 * 
-	 * @param context
-	 *            application's context.
-	 * @param regId
-	 *            registration ID
-	 */
-	private void storeRegistrationId(Context context, String regId) {
-		final SharedPreferences prefs = getGcmPreferences(context);
-		int appVersion = getAppVersion(context);
-		Log.i(TAG, "Saving regId on app version " + appVersion);
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(PROPERTY_REG_ID, regId);
-		editor.putInt(PROPERTY_APP_VERSION, appVersion);
-		editor.commit();
-	}
 
-	/**
-	 * Gets the current registration ID for application on GCM service, if there
-	 * is one.
-	 * <p>
-	 * If result is empty, the app needs to register.
-	 * 
-	 * @return registration ID, or empty string if there is no existing
-	 *         registration ID.
-	 */
-	private String getRegistrationId(Context context) {
-		final SharedPreferences prefs = getGcmPreferences(context);
-		String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-		if (registrationId.isEmpty()) {
-			Log.i(TAG, "Registration not found.");
-			return "";
-		}
-		// Check if app was updated; if so, it must clear the registration ID
-		// since the existing regID is not guaranteed to work with the new
-		// app version.
-		int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION,
-				Integer.MIN_VALUE);
-		int currentVersion = getAppVersion(context);
-		if (registeredVersion != currentVersion) {
-			Log.i(TAG, "App version changed.");
-			return "";
-		}
-		return registrationId;
-	}
+
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 	}
 
-	/**
-	 * @return Application's version code from the {@code PackageManager}.
-	 */
-	private static int getAppVersion(Context context) {
-		try {
-			PackageInfo packageInfo = context.getPackageManager()
-					.getPackageInfo(context.getPackageName(), 0);
-			return packageInfo.versionCode;
-		} catch (NameNotFoundException e) {
-			// should never happen
-			throw new RuntimeException("Could not get package name: " + e);
-		}
-	}
-
-	/**
-	 * @return Application's {@code SharedPreferences}.
-	 */
-	private SharedPreferences getGcmPreferences(Context context) {
-		// This sample app persists the registration ID in shared preferences,
-		// but
-		// how you store the regID in your app is up to you.
-		return getSharedPreferences(DemoActivity.class.getSimpleName(),
-				Context.MODE_PRIVATE);
-	}
 }
